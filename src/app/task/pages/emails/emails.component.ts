2647fd0c5274081d5fdbf3fda1da9884157835bf
git\Enrique Bernardo González Vázquez <ebgvdeveloper@gmail.com>
2024-11-08 00:10:59 -0600

    Add email management and template management features: implement email listing with dynamic data binding, create form for managing templates with validation, and integrate API calls for fetching and saving template data

--- src/app/task/pages/emails/emails.component.ts
+++ src/app/task/pages/emails/emails.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { EmailService } from 'src/app/services/email.service';
 
 @Component({
   selector: 'app-emails',
   templateUrl: './emails.component.html',
-  styleUrls: ['./emails.component.css']
+  styleUrls: ['./emails.component.css'],
 })
-export class EmailsComponent {
+export class EmailsComponent implements OnInit {
+  emails: any[] = [];
 
+  constructor(private emailService: EmailService) {}
+
+  ngOnInit(): void {
+    this.getEmails();
+  }
+
+  getEmails(): void {
+    this.emailService.getEmails().subscribe((data) => {
+      this.emails = data;
+    });
+  }
 }
--- src/app/task/pages/manage-template/manage-template.component.ts
+++ src/app/task/pages/manage-template/manage-template.component.ts
@@ -1,10 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-manage-template',
   templateUrl: './manage-template.component.html',
-  styleUrls: ['./manage-template.component.css']
+  styleUrls: ['./manage-template.component.css'],
 })
-export class ManageTemplateComponent {
+export class ManageTemplateComponent implements OnInit {
+  templateForm: FormGroup;
+  templateId: number | null = null;
 
+  constructor(private fb: FormBuilder, private http: HttpClient) {
+    this.templateForm = this.fb.group({
+      title: ['', Validators.required],
+      subtitle: ['', Validators.required],
+      description: ['', Validators.required],
+      logoSrc: ['', Validators.required],
+      rectorName: ['', Validators.required],
+      rectorSignatureSrc: ['', Validators.required],
+    });
+  }
+
+  ngOnInit(): void {
+    this.loadTemplate();
+  }
+
+  loadTemplate(): void {
+    this.http.get<any>('API_ENDPOINT/templates').subscribe((data) => {
+      if (data && data.length > 0) {
+        const template = data[0];
+        this.templateId = template.id;
+        this.templateForm.patchValue(template);
+      }
+    });
+  }
+
+  onSubmit(): void {
+    if (this.templateForm.valid) {
+      const templateData = this.templateForm.value;
+      if (this.templateId) {
+        this.http
+          .put(`API_ENDPOINT/templates/${this.templateId}`, templateData)
+          .subscribe((response) => {
+            console.log('Template updated', response);
+          });
+      } else {
+        this.http
+          .post('API_ENDPOINT/templates', templateData)
+          .subscribe((response) => {
+            console.log('Template created', response);
+          });
+      }
+    }
+  }
 }

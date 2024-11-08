4bcb2c9bcf4d130145b82fb212c7695a975b8e39
git\Enrique Bernardo González Vázquez <ebgvdeveloper@gmail.com>
2024-11-08 01:51:46 -0600

    Add email generation functionality: implement method to send birthday emails to teachers with confirmation using SweetAlert

--- src/app/task/pages/emails/emails.component.ts
+++ src/app/task/pages/emails/emails.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { EmailService } from 'src/app/services/email.service';
+import { TeacherService } from 'src/app/services/teacher.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-emails',
@@ -9,7 +11,10 @@ import { EmailService } from 'src/app/services/email.service';
 export class EmailsComponent implements OnInit {
   emails: any[] = [];
 
-  constructor(private emailService: EmailService) {}
+  constructor(
+    private emailService: EmailService,
+    private teacherService: TeacherService
+  ) {}
 
   ngOnInit(): void {
     this.getEmails();
@@ -18,6 +23,33 @@ export class EmailsComponent implements OnInit {
   getEmails(): void {
     this.emailService.getEmails().subscribe((data) => {
       this.emails = data;
+    });
+  }
+
+  generateEmail(emailData: any): void {
+    // 1. Obtener datos del email
+    const message = emailData.message;
+    const teacherId = emailData.teacherId;
+    // 2. Obtener datos del profesor
+    this.teacherService.getTeacherById(teacherId).subscribe((teacherData) => {
+      const teacherEmail = teacherData.email;
+      const teacherName = teacherData.name;
+      // 3. Send email
+      this.emailService
+        .sendEmailToAnyPerson({
+          to: teacherEmail,
+          subject: 'Felicitación de cumpleaños',
+          message: message,
+          name: teacherName,
+        })
+        .subscribe(() => {
+          console.log('Email sent');
+          Swal.fire(
+            'Email enviado',
+            'El email ha sido enviado al profesor con éxito',
+            'success'
+          );
+        });
     });
   }
 }
